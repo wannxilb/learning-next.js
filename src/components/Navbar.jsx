cdb22357b8ffd28e230313fea64b31ec16f5b802
windxwann <w4919878@gmail.com>
2026-07-08 15:16:22 +0700

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,43 @@
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <nav className="bg-white">
+      <div className="flex items-center justify-between w-full max-w-[1130px] py-[22px] mx-auto">
+        <Link href="/">
+          <img src="/assets/images/logos/logo.svg" alt="logo" />
+        </Link>
+        <ul className="flex items-center gap-[50px] w-fit">
+          <li>
+            <Link href="/">Browse</Link>
+          </li>
+          <li>
+            <Link href="/">Popular</Link>
+          </li>
+          <li>
+            <Link href="/">Categories</Link>
+          </li>
+          <li>
+            <Link href="/">Events</Link>
+          </li>
+          <li>
+            <Link href="/">My Booking</Link>
+          </li>
+        </ul>
+        <a
+          href="#"
+          className="flex items-center gap-[10px] rounded-full border border-[#000929] py-3 px-5"
+        >
+          <img
+            src="/assets/images/icons/call.svg"
+            className="w-6 h-6"
+            alt="icon"
+          />
+          <span className="font-semibold">Contact Us</span>
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
